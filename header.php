a689c344e8ab1979a4cf5241019dd68daa363964
Hassan Mahmood <39520666+hassanfsd@users.noreply.github.com>
2018-05-23 16:55:23 +0500

    Revert "Merge pull request #1 from hassanfsd/IHTESHAMULHAQ-patch-1"
    This reverts commit df89372af0816d8d6a21be3f0fa9fa556d6d5489, reversing
    changes made to 6f42979bc2413cf652508a0e28e14e76ff8e8e0a.

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>GCUF</title>
+<title>Click-Technologies</title>
 <link rel="Shortcut Icon" href="favicon.ico" type="image/x-icon" />
 <meta charset="utf-8"><link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
